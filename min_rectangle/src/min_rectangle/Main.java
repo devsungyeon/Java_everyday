490ee8cd4ae8c592aee760ea3e7a749cc44bede2
devsungyeon <sungyeonlee1103@gmail.com>
2021-10-25 19:12:38 +0900

    findPrime done

---
+++ min_rectangle/src/min_rectangle/Main.java
@@ -0,0 +1,13 @@
+package min_rectangle;
+
+public class Main {
+	public static void main(String[] args) {
+		int[][] sizes = {};
+		
+		Solution sol = new Solution();
+		
+		int result = sol.solution(sizes);
+		
+		System.out.println(result);
+	}
+}
---
+++ min_rectangle/src/min_rectangle/Solution.java
@@ -0,0 +1,16 @@
+package min_rectangle;
+
+public class Solution {
+	public int solution(int[][] sizes) {
+        int answer = 0;
+        
+        int[] width;
+        int[] length;
+        for(int[] size:sizes) {
+        	
+        }
+        
+        
+        return answer;
+    }
+}
